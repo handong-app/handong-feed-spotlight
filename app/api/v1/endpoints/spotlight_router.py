860c594b51f6de028d2997f128f7a9e24533bbd5
callendev20 <callendev20@gmail.com>
2025-03-19 14:25:00 +0900

    :sparkles: Implement SpotlightRouter

---
+++ app/api/v1/endpoints/spotlight_router.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.core.database import get_db
+from app.schemas.spolight_dto import SpotlightDto
+from app.services.spotlight_service import SpotlightService
+
+
+class SpotlightRouter:
+
+    def __init__(self):
+        self.router = APIRouter()
+
+        self.router.add_api_route("/{target_date}", self.get_spotlight, methods=["GET"])
+
+    def get_spotlight(self, target_date: str, db: Session = Depends(get_db)) -> SpotlightDto.GetSpotlightRespDto:
+        """target_date 의 spotlight 를 get"""
+        service = SpotlightService(db)
+        get_spotlight_resp_dto = service.get_spotlight(SpotlightDto.GetSpotlightReqDto(target_date=target_date))
+        return  get_spotlight_resp_dto
+
+
+
+spotlight_router = SpotlightRouter().router
