58418aca41b065fb64289331bdeb34ef56c5f75d
callendev20 <callendev20@gmail.com>
2025-03-19 14:20:45 +0900

    :white_check_mark: Revise FeedMessageDto - commented out last_sent_at_datetime filed

--- app/schemas/feed_message_dto.py
+++ app/schemas/feed_message_dto.py
@@ -6,4 +6,4 @@ class FeedMessageDto(BaseModel):
     """ FetchFeedMessagesByDateServDto 의 messages (배열) 필드에 들어갈 요소의 타입을 지정해주는 역할"""
     id: str
     message: str
-    last_sent_at_datetime: str
+    # last_sent_at_datetime: str
