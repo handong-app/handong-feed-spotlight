126a58461347d64ebf6056f9af4f913b3f0d742f
callendev20 <callendev20@gmail.com>
2025-03-19 13:14:42 +0900

    :white_check_mark: Add SpotlightDto

---
+++ app/schemas/spolight_dto.py
@@ -0,0 +1,22 @@
+from typing import Dict, List
+from pydantic import BaseModel
+
+from app.schemas.feed_message_dto import FeedMessageDto
+
+
+class SpotlightDto:
+    class GetSpotlightReqDto(BaseModel):
+        target_date: str
+
+    class GetSpotlightRespDto(BaseModel):
+        target_date: str
+        # summary: str
+        spotlights: List[Dict[str, str]]
+
+    class FetchFeedMessagesByDateServDto(BaseModel):
+        target_date: str
+        messages:  List[FeedMessageDto]
+
+    class GenerateSpotlightRankServDto(BaseModel):
+        spotlights: List[Dict[str, str]]
+
