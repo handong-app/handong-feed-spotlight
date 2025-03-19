4f70f12f876a2aa7f5bd7dc5a37fbaff2c853b0e
callendev20 <callendev20@gmail.com>
2025-03-19 14:26:23 +0900

    :white_check_mark: Add spotlight_router to router.py

--- app/api/router.py
+++ app/api/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from app.api.v1.endpoints.spotlight_router import spotlight_router
 from app.api.v1.endpoints.test_router  import test_router
 from app.api.v1.endpoints.healthcheck_router import healthcheck_router
 
@@ -9,5 +10,6 @@ api_v1_router = APIRouter(prefix="/v1")
 
 api_v1_router.include_router(test_router, prefix="/test", tags=["Test"])
 api_v1_router.include_router(healthcheck_router, prefix="/healthcheck", tags=["Healthcheck"])
+api_v1_router.include_router(spotlight_router, prefix="/spotlight", tags=["Spotlight"])
 
 api_router.include_router(api_v1_router)
