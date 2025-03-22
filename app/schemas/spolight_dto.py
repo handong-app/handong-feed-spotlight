b19b50cce839f71af4df56056c4ff56e75f764f2
callendev20 <callendev20@gmail.com>
2025-03-23 01:00:58 +0900

    :white_check_mark: Add GetSpotlightRespDto and GenerateSpotlightScoreServDto - SpotlightDto

--- app/schemas/spolight_dto.py
+++ app/schemas/spolight_dto.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import List
 from pydantic import BaseModel
 
 from app.schemas.feed_message_dto import FeedMessageDto
@@ -9,13 +9,16 @@ class SpotlightDto:
         target_date: str
 
     class GetSpotlightRespDto(BaseModel):
-        target_date: str
-        # summary: str
-        # spotlights: List[Dict[str, str]]
-        scores: List[int]
+        for_date: str
+        summary: str
+        scores: List["SpotlightDto.GenerateSpotlightScoreServDto"]
 
     class FetchFeedMessagesByDateServDto(BaseModel):
         target_date: str
         messages:  List[FeedMessageDto]
 
     class GenerateSpotlightScoreServDto(BaseModel):
+        tb_ka_message_id: str
+        score: int
+
+SpotlightDto.GetSpotlightRespDto.model_rebuild()
