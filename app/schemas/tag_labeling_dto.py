4044c798171ffd91541ef97d158d939d5cf1ff2e
callendev20 <callendev20@gmail.com>
2025-04-15 23:18:48 +0900

    :recycle: refactor: modify type of assign_resp_dtos_list to List[List[SubjectTagDto.AssignRespDto]] in MessageTagLabelingRespDto

--- app/schemas/tag_labeling_dto.py
+++ app/schemas/tag_labeling_dto.py
@@ -1,10 +1,13 @@
 from pydantic import BaseModel
 from typing import List
 
+from app.schemas.external.subject_tag_dto import SubjectTagDto
+
+
 class MessageTagAssignment(BaseModel):
     subject_id: str
     tag_codes: List[str]
 
 class MessageTagLabelingRespDto(BaseModel):
-    assignments: List[MessageTagAssignment]
+    assign_resp_dtos_list: List[List[SubjectTagDto.AssignRespDto]]
     
