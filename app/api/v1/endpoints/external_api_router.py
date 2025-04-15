7420f05ac1ac5412a325792939455df05393d408
callendev20 <callendev20@gmail.com>
2025-04-15 18:30:16 +0900

    :white_check_mark: feat: add assign_tag and assign_tags_batch endpoint

--- app/api/v1/endpoints/external_api_router.py
+++ app/api/v1/endpoints/external_api_router.py
@@ -3,6 +3,7 @@ from typing import List
 from fastapi import APIRouter
 
 from app.schemas.external.feed_dto import FeedDto
+from app.schemas.external.subject_tag_dto import SubjectTagDto
 from app.schemas.external.tag_dto import TagDto
 from app.clients.handong_feed_app_client import HandongFeedAppClient
 
@@ -17,3 +18,13 @@ def get_all_tags():
 def get_feeds(req: FeedDto.ReadReqDto):
     service = HandongFeedAppClient()
     return service.get_feeds(req)
+
+@external_api_router.post("/assign-tag/{subject_id}", response_model=SubjectTagDto.AssignRespDto)
+def assign_tag(subject_id: str, assign_req: SubjectTagDto.AssignReqDto):
+    service = HandongFeedAppClient()
+    return service.assign_tag(subject_id, assign_req)
+
+@external_api_router.post("/assign-tag/{subject_id}/batch", response_model=List[SubjectTagDto.AssignRespDto])
+def assign_tags_batch(subject_id: str, assign_reqs: List[SubjectTagDto.AssignReqDto]):
+    service = HandongFeedAppClient()
+    return service.assign_tags_batch(subject_id, assign_reqs)
