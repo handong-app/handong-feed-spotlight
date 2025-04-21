96e95ae6bec14123de000579e86616131771c95d
callendev20 <callendev20@gmail.com>
2025-04-21 23:49:31 +0900

    :white_check_mark: feat: add AssignTagsToMessageServDto

--- app/schemas/tag_labeling_dto.py
+++ app/schemas/tag_labeling_dto.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
+from datetime import date
 
 from app.schemas.external.subject_tag_dto import SubjectTagDto
 
@@ -10,4 +11,10 @@ class MessageTagAssignment(BaseModel):
 
 class MessageTagLabelingRespDto(BaseModel):
     assign_resp_dtos_list: List[List[SubjectTagDto.AssignRespDto]]
+
+class AssignTagsToMessageServDto(BaseModel):
+    fail_id: Optional[int] = None
+    subject_id: str
+    for_date: str
+    message: str
     
