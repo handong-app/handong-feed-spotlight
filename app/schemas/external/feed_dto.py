f092b0ad30e559d235474261f3a876c4dcf9b55b
callendev20 <callendev20@gmail.com>
2025-04-21 23:53:13 +0900

    :white_check_mark: feat: add to_assign_tags_to_message_serv_dto() - external/feed_dto

--- app/schemas/external/feed_dto.py
+++ app/schemas/external/feed_dto.py
@@ -1,12 +1,15 @@
 from pydantic import BaseModel, field_validator
 from typing import Optional, List
 
+from app.schemas.tag_labeling_dto import AssignTagsToMessageServDto
+from app.util.date_utils import convert_unix_to_date_str
+
 
 class FeedDto:
     class ReadReqDto(BaseModel):
         start: Optional[int] = None
         end: Optional[int] = None
-        isFilterNew: bool = False
+        isFilterNew: int = 1
         limit: Optional[int] = None
 
     class ReadRespDto(BaseModel):
@@ -21,3 +24,10 @@ class FeedDto:
         @field_validator("files", mode="before")
         def default_files(cls, value):
             return value or []
+
+        def to_assign_tags_to_message_serv_dto(self) -> AssignTagsToMessageServDto:
+            return AssignTagsToMessageServDto(
+                subject_id=str(self.subjectId),
+                for_date=convert_unix_to_date_str(self.sentAt),
+                message=self.message,
+            )
