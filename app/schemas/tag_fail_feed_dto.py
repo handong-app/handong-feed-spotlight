77d5373cea96ef641fccf21c886579c88e32233c
callendev20 <callendev20@gmail.com>
2025-04-21 23:52:52 +0900

    :white_check_mark: feat: add to_assign_tags_to_message_serv_dto() and to_assign_tags_to_message_serv_dto_list() - tag_fail_feed_dto

--- app/schemas/tag_fail_feed_dto.py
+++ app/schemas/tag_fail_feed_dto.py
@@ -2,6 +2,8 @@ from typing import List
 from pydantic import BaseModel
 from datetime import date
 
+from app.schemas.tag_labeling_dto import AssignTagsToMessageServDto
+
 
 class FailFeedDetail(BaseModel):
     """
@@ -12,9 +14,18 @@ class FailFeedDetail(BaseModel):
         message (str): 실패한 메시지의 내용.
         for_date (date): 실패한 메시지의 날짜.
     """
+    fail_id: int
     subject_id: int
     message: str
     for_date: date
+
+    def to_assign_tags_to_message_serv_dto(self) -> AssignTagsToMessageServDto:
+        return AssignTagsToMessageServDto(
+            fail_id=self.fail_id,
+            subject_id=str(self.subject_id),
+            for_date=str(self.for_date),
+            message=self.message,
+        )
 
 
 class FailFeedResp(BaseModel):
@@ -25,3 +36,8 @@ class FailFeedResp(BaseModel):
         fail_feeds (List[FailFeedDetail]): 각 실패 피드의 상세 정보를 포함하는 리스트.
     """
     fail_feeds: List[FailFeedDetail]
+
+    def to_assign_tags_to_message_serv_dto_list(self) -> List[AssignTagsToMessageServDto]:
+        return [
+            dto.to_assign_tags_to_message_serv_dto() for dto in self.fail_feeds
+        ]
