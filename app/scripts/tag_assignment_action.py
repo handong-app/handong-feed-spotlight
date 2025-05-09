e73166834430a0c66f1bb8214563fb2498d270a4
callendev20 <callendev20@gmail.com>
2025-05-09 15:29:03 +0900

    :recycle: refactor: rename app/scripts/tag_assignment_cron_job.py to tag_assignment_action.py

--- app/scripts/tag_assignment_action.py
+++ app/scripts/tag_assignment_action.py
@@ -21,7 +21,7 @@ def run():
         yesterday = (date.today() - timedelta(days=1)).isoformat()
         latest_for_date = str(client.get_latest_for_date().latestForDate + timedelta(days=1))
 
-        print(f"[CRON] Assigning tags to new feeds for {latest_for_date} ~ {yesterday}")
+        print(f"[ACTION] Assigning tags to new feeds for {latest_for_date} ~ {yesterday}")
         try:
             resp = service.process_feeds_with_date(
                 start_date=latest_for_date,
@@ -30,30 +30,30 @@ def run():
                 limit=100
             )
             print(
-                f"[CRON] Number of new feeds added yesterday that were successfully assigned: {len(resp.assign_resp_dtos_list)}")
+                f"[ACTION] Number of new feeds added yesterday that were successfully assigned: {len(resp.assign_resp_dtos_list)}")
 
 
         except HTTPException as e:
             if e.status_code == 204:
-                print(f"[CRON] No new feeds to process for {yesterday}.")
+                print(f"[ACTION] No new feeds to process for {yesterday}.")
             else:
                 raise e
 
 
-        print("\n[CRON] Retrying failed tag assignments...")
+        print("\n[ACTION] Retrying failed tag assignments...")
         try:
             resp =  service.process_failed_feeds()
-            print(f"[CRON] Number of failed feeds that were successfully assigned by retry: {len(resp.assign_resp_dtos_list)}")
+            print(f"[ACTION] Number of failed feeds that were successfully assigned by retry: {len(resp.assign_resp_dtos_list)}")
 
 
         except HTTPException as e:
             if e.status_code == 204:
-                print("[CRON] No failed feeds to process.")
+                print("[ACTION] No failed feeds to process.")
             else:
                 raise e
 
     except Exception as e:
-        logging.error(f"[CRON] Error during tag assignment: {e}", exc_info=True)
+        logging.error(f"[ACTION] Error during tag assignment: {e}", exc_info=True)
 
     finally:
         db.close()
